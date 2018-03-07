3b29107940f0b95f0e739402e12edb914d8ccbdc
Monjuma Begum <Monjuma.Begum@cloudhouse.com>
2018-03-07 16:18:52 +0000

    added project

---
+++ Practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Student studentAge = new Student();
+            //studentAge.Age();
+
+            //Employee workers = new Employee();
+            //workers.EmployeeInfo();
+
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+
+            int average = (num1 + num2 + num3 + num4) / 4;
+            Console.WriteLine("Average is: " + average);
+
+        }
+    }
+}
---
+++ Practice/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Student
+    {
+        public void Age()
+        {
+            DateTime dateOfBirth = Convert.ToDateTime("1998/08/07");
+            string age = CalculateAge(dateOfBirth);
+
+            Console.WriteLine("You are " + age + " years old.");
+
+
+            string CalculateAge(DateTime birthDate)
+            {
+                int years = DateTime.Now.Year - birthDate.Year;
+
+                if (birthDate.Month > DateTime.Now.Month || birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day)
+                {
+                    years--;
+                }
+
+                return years.ToString();
+            }
+
+        }
+       
+       
+    }
+}
