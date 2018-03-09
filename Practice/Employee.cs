28fd636f0f46f169413f10d4994f194a47ecbd31
Monjuma Begum <Monjuma.Begum@cloudhouse.com>
2018-03-09 11:05:39 +0000

    Added Employee class

---
+++ Practice/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Employee
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Salary { get; }
+
+        public Employee(int id, string name, int salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        //public string EmpInfo()
+        //{
+            
+            
+        //}
+
+        public override string ToString()
+        {
+            return $"Employee ID: {Id}, Name: {Name}, Salary: {Salary}.";
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,44 @@ namespace Practice
     {
         public static void Main(string[] args)
         {
-            var student1 = new Student("Amy", Convert.ToDateTime("2000/02/18"));
-            student1.Age();
+            //var student1 = new Student("Amy", Convert.ToDateTime("2000/02/18"));
+            //student1.Age();
+            //Console.WriteLine(student1);
 
-            Console.WriteLine(student1);
+            //var student2 = new Student("Tom", Convert.ToDateTime("1982/04/17"));
+            //student2.Age();
+            //Console.WriteLine(student2);
 
-            var student2 = new Student("Tom", Convert.ToDateTime("1982/04/17"));
-            student2.Age();
+            List<Employee> newEmployee = new List<Employee>
+            {
+                
+                new Employee(1, "John", 30000),
+                new Employee(2, "Charlotte", 20000),
+                new Employee(3, "Jacob", 15000),
+                new Employee(4, "Jim", 89250),
+                new Employee(5, "Mark", 652),
+                new Employee(6, "Chris", 4569),
+                new Employee(7, "Steve", 54180),
+                new Employee(8, "Rory", 54681)
+            };
 
-            Console.WriteLine(student2);
+            var currentSalary = 0;
+
+            foreach (Employee employee in newEmployee)
+            {
+                if (employee.Salary > currentSalary)
+                {
+                    currentSalary = employee.Salary;
+                }
+   
+            }
+            Console.WriteLine(currentSalary);
+
+            //foreach (Employee employee in newEmployee)
+            //{
+
+            //}
+
         }
 
     }
