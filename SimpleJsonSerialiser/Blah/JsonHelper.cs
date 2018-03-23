f61075c671471012be4dd243bb822c18c576f68c
Monjuma Begum <Monjuma.Begum@cloudhouse.com>
2018-03-23 14:29:30 +0000

    simple json serialiser

---
+++ SimpleJsonSerialiser/Blah/JsonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JsonTraining
+{
+    public static class JsonHelper
+    {
+        public static string JsonSerialise(IEnumerable<int> numbers)
+        {
+
+            var numberList = numbers.ToList();
+            var result = "{ \"numbers\":[ ";
+
+            for (int i = 0; i < numberList.Count(); i++)
+            {
+                if (i == (numberList.Count() - 1))
+                {
+                    result += numberList[i];
+                }
+                else
+                {
+                    result += numberList[i] + ", ";
+                }               
+            }
+            result += " ] }";
+
+            return result;
+        }
+    }
+}
---
+++ SimpleJsonSerialiser/Test/JsonHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonTraining;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class JsonHelperTest
+    {
+        [Test]
+        public void Json_serialise()
+        {
+            var numberArray = new []
+            {
+                1, 2, 4, 3, 5
+            };
+
+            var json = JsonHelper.JsonSerialise(numberArray);
+
+            var expectedResult = "{ \"numbers\":[ 1, 2, 4, 3, 5 ] }";
+
+            Console.WriteLine($"Actual result: {json} and expected result is {expectedResult}");
+
+            Assert.AreEqual(expectedResult, json);
+        }
+    }
+}
