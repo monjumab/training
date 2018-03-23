08699e39196326a29df565a64666d9356a2cdcfe
Monjuma Begum <Monjuma.Begum@cloudhouse.com>
2018-03-23 14:17:54 +0000

    Project finding unique words in sentence

---
+++ UnqiueWords/UniqueWordsTest/StringParseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using UnqiueWords;
+
+namespace UniqueWordsTest
+{
+    [TestFixture]
+    public class StringParseTest
+    {
+        [Test]
+        public void UniqueWordInString()
+        {
+           StringParser words = new StringParser();
+           List<string> unique = words.UniqueWords("Looking for C# developer. To become a c# DeveloPer you need to work hard");
+         
+           Assert.AreEqual(new List<string>{"Looking", "for", "become", "a", "you", "need", "work", "hard"}, unique);
+    
+        }
+
+    }
+}
---
+++ UnqiueWords/UnqiueWords/StringParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnqiueWords
+{
+    public class StringParser
+    {
+        public List<string> UniqueWords(string word)
+        {
+            word = word.Replace(".", string.Empty);
+            List<string> individualWords = word.Split(' ').ToList();
+
+            foreach (var individualWord in individualWords)
+            {
+                Console.WriteLine(individualWord);
+            }
+            individualWords.ForEach(Console.WriteLine);
+
+            var individual = individualWords.GroupBy(g => g, StringComparer.CurrentCultureIgnoreCase).Where(j => j.Count() == 1);
+            return individual.Select(i => i.Key).ToList();
+        }
+    }
+}
